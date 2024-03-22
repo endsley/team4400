344b62ddece12270bafbf0441f3ce9a0e50f903f
anushkan-wani <anushkan.wani@gmail.com>
2024-03-22 19:33:23 -0400

    Renamed Assignment 0 question, inprogress Chapter 1 question

--- C1.Q1.7.py
+++ C1.Q1.7.py
@@ -4,39 +4,59 @@ from utils import *
 if __name__ == "__main__":
     n_sec = 100
 
-    def display():
-        def display_problem():
-            display_color_problem_title()
-            [random1_val1, random1_val2, random1_val3, random2_val1, random2_val2, random3_val3] = retrieve_display_variables()
-            display_new_line(r''' Given \\ 
-\begin{equation*}
-  x = \begin{bmatrix}
-    %d \\
-    %d\\
-    %d
-\end{bmatrix},   
-  y = \begin{bmatrix}
-    %d \\
-    %d \\
-    %d
-\end{bmatrix},   
-\end{equation*}
-Calculate the following: $\langle y, x \rangle$''', 
-                             (random1_val1, random1_val2, random1_val3, random2_val1, random2_val2, random3_val3))
+    def generate_random_matrix(rows, cols, min_val, max_val):
+        # Generates a matrix with random integers of the specified size
+        return np.random.randint(min_val, max_val + 1, size=(rows, cols))
 
+
+    def display_matrix_question():
+
+
+        def define_matrices():
+            x = generate_random_matrix(3, 1, 0, 5)
+            y = generate_random_matrix(3, 1, 0, 5)
+            return x, y
+
+        def matrix_to_latex(matrix):
+            if matrix.ndim == 1:
+                matrix = matrix[:, None]  # Convert 1D array to column vector
+            return r'\begin{bmatrix}' + r' \\ '.join(
+                ' & '.join(str(cell) for cell in row) for row in matrix) + r'\end{bmatrix}'
+
+
+
+        def display_problem(x, y):
+            # Convert numpy arrays to LaTeX bmatrix format.
+
+            x_latex = matrix_to_latex(x)
+            y_latex = matrix_to_latex(y)
+            
+            display_new_line(
+                r'''Given vectors with random elements:''')
+            display_new_line(
+                r'''$x = %s$ and $y = %s$ are vectors.''' % (
+                    x_latex, y_latex))
+            display_new_line(
+                r'''Calculate $\langle y, x \rangle$.''')
 
         if problem_has_been_answered():
-            display_problem()
+            display_problem(x, y)
             params = display_solutions()
         else:
-            [random1_val1, random1_val2, random1_val3, random2_val1, random2_val2, random3_val3] = getMultInts(6, 1, 3)
-            set_page_variables_for_display([random_addon, random_coefficient, power, random_lower, random_upper])
-            display_problem()
-            def anti_derivative(x):
-                return random_coefficient * (1 / (power + 1)) * x**(power + 1) + random_addon*x
-            def solve_anti_deriv():
-                return anti_derivative(random_upper) - anti_derivative(random_lower)
-            solution = solve_anti_deriv()
+            [random1_val1, random1_val2, random1_val3, random2_val1, random2_val2, random2_val3] = getMultInts(6, 1, 3)
+            set_page_variables_for_display([random1_val1, random1_val2, random1_val3, random2_val1, random2_val2, random2_val3])
+            display_problem(x, y)
+            x = np.array([[random1_val1],
+                          [random1_val2],
+                          [random1_val3]])
+            y = np.array([[random2_val1],
+                          [random2_val2],
+                          [random2_val3]])
+            
+            def inner_product(x, y):
+                return y.T.dot(x)
+            
+            solution = inner_product(x, y).item()
             params = problem_display_ending(solution, 'number')
 
             if 'preview' in st.session_state and st.session_state['preview'] == 1:
@@ -44,5 +64,5 @@ Calculate the following: $\langle y, x \rangle$''',
 
         return params
 
-    standard_problem_page(n_sec, display)
+    standard_problem_page(n_sec, display_matrix_question)
 
