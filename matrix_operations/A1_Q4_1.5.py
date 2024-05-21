c6de3ce76cdaffa42c7e67415dc512c8cabe0191
veroaaguiar <aguiarortega.ve@husky.neu.edu>
2024-05-21 18:18:44 -0400

    Create A1_Q4_1.5.py

---
+++ matrix_operations/A1_Q4_1.5.py
@@ -0,0 +1,57 @@
+import numpy as np
+import streamlit as st
+from utils import *
+
+if __name__ == "__main__":
+    n_sec = 150
+
+
+    def display():
+        def display_problem():
+            display_color_problem_title()
+            (x00, x01, x10, x11, x20, x21, x30, x31) = retrieve_display_variables()
+            display_new_line(
+                r''' 
+                Given the following data:
+                '''
+            )
+            display_new_line(
+                r''' 
+                $\begin{bmatrix}
+                x_{1} & x_{2} \\
+                %d & %d \\
+                %d & %d \\
+                %d & %d \\
+                %d & %d
+                \end{bmatrix}$
+                ''',
+                (x00, x01, x10, x11, x20, x21, x30, x31)
+            )
+            display_new_line(
+                r''' 
+                Transform the data using the feature map: $\phi_{5}(x)= x_{2}^2$. 
+                What is the first element in $\phi_{5}(x)$?
+                '''
+            )
+
+        if problem_has_been_answered():
+            display_problem()
+            params = display_solutions()
+        else:
+            # get values for matrix and display
+            mat_vals = getMultInts(8, -4, 4)
+            (x00, x01, x10, x11, x20, x21, x30, x31) = mat_vals
+            set_page_variables_for_display([x00, x01, x10, x11, x20, x21, x30, x31])
+            display_problem()
+
+            # calculate the estimated solution
+            solution = x00
+            params = problem_display_ending(solution, 'number')
+
+            if 'preview' in st.session_state and st.session_state['preview'] == 1:
+                st.write("Solution is: " + str(solution))
+
+        return params
+
+
+    standard_problem_page(n_sec, display)
